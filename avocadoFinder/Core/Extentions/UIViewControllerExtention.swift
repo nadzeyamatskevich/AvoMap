6cc27cd49762f7a8a6a4754f1f853976309b3ec2
nadzeyamatskevich <nadezhda.matskevich.95@google.com>
2019-06-25 22:09:16 +0300

    news with server

---
+++ avocadoFinder/Core/Extentions/UIViewControllerExtention.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewControllerExtention.swift
+//  avocadoFinder
+//
+//  Created by Nadzeya Savitskaya on 6/25/19.
+//  Copyright © 2019 Nadzeya Savitskaya. All rights reserved.
+//
+
+import Foundation
---
+++ avocadoFinder/Group/OneNewsInfo/Manager/Layout/OneNewsInfoLayoutManager.swift
@@ -0,0 +1,9 @@
+//
+//  OneNewsInfoLayoutManager.swift
+//  avocadoFinder
+//
+//  Created by Nadzeya Savitskaya on 6/25/19.
+//  Copyright © 2019 Nadzeya Savitskaya. All rights reserved.
+//
+
+import Foundation
