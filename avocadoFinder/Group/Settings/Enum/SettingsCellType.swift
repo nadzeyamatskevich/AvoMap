6bb1446b62e4fffa7e4863d9b6e679991b31b701
nadzeyamatskevich <nadezhda.matskevich.95@google.com>
2019-07-14 16:19:28 +0300

    build 1.0

---
+++ avocadoFinder/Group/Settings/Enum/SettingsCellType.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsCellType.swift
+//  avocadoFinder
+//
+//  Created by Nadzeya Savitskaya on 7/14/19.
+//  Copyright © 2019 Nadzeya Savitskaya. All rights reserved.
+//
+
+import Foundation
